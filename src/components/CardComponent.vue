9e4530fd97444f027d7cc91165e0c4b59dc59a56
adrisala20 <adriana_15_tuya@hotmail.com>
2024-04-11 17:19:18 +0200

    added cards in main

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,20 @@
+<template>
+        <div class="card" >
+             <img :src="image" class="card-img-top" :alt="title">
+             <div class="card-body">
+                 <h5 class="card-title">{{ title }}</h5>
+             </div>
+        </div>
+</template>
+
+<script>
+    export default {
+        name:'CardComponent',
+        props:['image','title','text', 'link']
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,15 +1,34 @@
 <template>
     <main >
         <div class="container">
-            >>> Contenuto <<<
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-2" v-for="(item,index) in comics" :key="index">
+                    <CardComponent :image="item.thumb" :title="item.series"  />
+                </div>
+            </div>
+
         </div>
         
     </main>
 </template>
 
 <script>
+import { comics } from '../components/data/store';
+import CardComponent from './CardComponent.vue'
+
     export default {
-        name:'MainComponent'
+        name:'MainComponent',
+        components: {
+            CardComponent,
+        },
+        data(){
+            return{
+                comics:comics
+            }
+        },
+        mounted(){
+            console.log(this.comics)
+        }
     }
 </script>
 
