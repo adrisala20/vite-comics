ba4874de65aebd46063f7da1976f9f38b30a7ede
adrisala20 <adriana_15_tuya@hotmail.com>
2024-04-11 15:54:23 +0200

    added footer top

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <HeaderComponent />
   <MainComponent />
+  <FooterComponentTop />
   <FooterComponent />
 
   <!--  non sono riuscita a finire la prima parte andrò avanti più tardi o domani  -->
@@ -11,14 +12,16 @@
   import HeaderComponent from './components/HeaderComponent.vue';
   import MainComponent from './components/MainComponent.vue';
   import FooterComponent from './components/FooterComponent.vue';
+  import FooterComponentTop from './components/FooterComponentTop.vue';
+  
   export default {
     name:'App',
     components:{
       HeaderComponent,
       MainComponent,
-      FooterComponent
+      FooterComponent,
+      FooterComponentTop,
     }
-    
   }
 </script>
 
---
+++ src/components/FooterComponentTop.vue
@@ -0,0 +1,76 @@
+<template>
+    <footer>
+        <div id="footer-top">
+            <div class="container">
+                <ul>
+                    <li v-for="(item,index) in navbar" :key="index">
+                        <img :src="item.img" :alt="item.title"> 
+                        <p> {{ item.title}}</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+    
+</template>
+
+<script>
+    export default {
+        name:'FooterComponentTop',
+        data(){
+            return{
+                navbar:[
+                    {
+                        title:'digital Comics',
+                        img:'/img/buy-comics-digital-comics.png'
+                    },
+                    {
+                        title:'dc merchandise',
+                        img:'/img/buy-comics-merchandise.png'
+                    },
+                    {
+                        title:'subscription',
+                        img:'/img/buy-comics-subscriptions.png'
+                    },
+                    {
+                        title:'come shop location',
+                        img:'/img/buy-comics-shop-locator.png'
+                    },
+                    {
+                        title:'dc power visa',
+                        img:'/img/buy-dc-power-visa.svg'
+                    },                   
+                ]
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    #footer-top{
+        background-color: #0282F9;
+        
+        ul{
+            list-style: none;
+            display: flex;
+            justify-content: start;
+            align-items: center;
+            width: 100%;
+                li{
+                    padding: 20px 15px;
+                    img{
+                        width: 4em;
+                        margin-right: 10px;
+                    };
+                    p{
+                        display: inline;
+                        text-transform: uppercase;
+                        color: white;
+                    }
+                }
+
+        }
+    }
+    
+
+</style>
