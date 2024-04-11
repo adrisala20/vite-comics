5f1a174ad1454ff0fd5ff44bb0a3b8ebff48fbc0
adrisala20 <adriana_15_tuya@hotmail.com>
2024-04-11 17:32:16 +0200

    added style to cards

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,7 +1,7 @@
 <template>
         <div class="card" >
              <img :src="image" class="card-img-top" :alt="title">
-             <div class="card-body">
+             <div class="card-body bg-black ">
                  <h5 class="card-title">{{ title }}</h5>
              </div>
         </div>
@@ -16,5 +16,17 @@
 </script>
 
 <style lang="scss" scoped>
+
+.card{
+    border: none;
+    cursor: pointer;
+    img{
+        height: 250px;
+    };
+    h5{
+        color: white;
+        text-transform: uppercase;
+    }
+}
 
 </style>
